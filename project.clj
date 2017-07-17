792f80cfe45713bdaf77d56cf3eb9512387a2c49
liquidz <liquidz.uo@gmail.com>
2017-07-17 07:20:54 +0000

    wip

--- project.clj
+++ project.clj
@@ -3,10 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ents/dtree "0.1.0-SNAPSHOT"]
-                 [ents/rforest "0.1.0-SNAPSHOT"]
-                 ]
   :plugins [[lein-sub "0.2.4"]
             [lein-codox "0.10.3"]
             ]
